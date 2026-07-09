3694df5fa8e16b1ff775a0390b4a87176a9d5826
Venkey-doki <venkatesdoki@gmail.com>
2026-07-09 12:57:19 +0530

    solved week2/js/meduim/problem - 3

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -9,5 +9,17 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const startTime = Date.now()
+    let sum = 0
+    for (let i = 0; i < n; i++) { 
+        sum = sum + i;
+    }
+    const endTime = Date.now()
+    return (endTime - startTime) - 1000;
 }
+
+
+console.log(calculateTime(100))
+console.log(calculateTime(1000000))
+console.log(calculateTime(10000000000))
+console.log(calculateTime(100000000000000))
