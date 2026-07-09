a14f45ae5c471641e33b1ddf6b7b77d481efc18c
Venkey-doki <venkatesdoki@gmail.com>
2026-07-09 13:09:26 +0530

    solved week2/js/hard/problem - 1

--- week-2/week-2-js/hard/todo-list.js
+++ week-2/week-2-js/hard/todo-list.js
@@ -11,7 +11,45 @@
 */
 
 class Todo {
+	constructor() { 
+		this.todos = [];
+	}
 
+	add(todo) { 
+		this.todos.push(todo)
+	}
+
+	remove(index) { 
+		if (index < 0 || index >= this.todos.length) { 
+			return "invalid"
+		}
+
+		this.todos.splice(index , 1)
+	}
+
+	update(index, todo) { 
+		if (index < 0 || index >= this.todos.length) {
+			return "invalid";
+		}
+		
+		this.todos[index] = todo
+	}
+
+	getAll() { 
+		return this.todos
+	}
+
+	get(index) { 
+		if (index < 0 || index >= this.todos.length) {
+			return null
+		}
+
+		return this.todos[index]
+	}
+
+	clear() { 
+		this.todos = [];
+	}
 }
 
 module.exports = Todo;
